dc525b002aa571ebb494a8b1928adf9b836edc2b
Kumar, Naveen (DS) <naveenkumar.anandraj@cdk.com>
2016-08-17 19:33:42 +0530

    More probs

---
+++ src/codeof/thirthyDays/Calculator.java
@@ -0,0 +1,20 @@
+package codeof.thirthyDays;
+
+/**
+ * Created by Naveen Kumar .A on 8/17/16.
+ */
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println(divisorSum(12));
+    }
+
+    public static int divisorSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
--- src/com/algo/SaveThePrisoner.java
+++ src/com/algo/SaveThePrisoner.java
@@ -1,6 +1,9 @@
 package com.algo;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Created by Naveen Kumar .A on 8/11/16.
@@ -18,6 +21,8 @@ public class SaveThePrisoner {
             if(poisonedSweet == 0 ){
                 poisonedSweet = n;
             }
+            Queue<Integer> ll = new LinkedList<>();
+            Stack<Integer> stack = new Stack<>();
             System.out.println(poisonedSweet);
         }
     }
