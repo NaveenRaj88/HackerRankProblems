ca78feb937312a94dbeeca9c5541d508e43f5efb
Kumar, Naveen (DS) <naveenkumar.anandraj@cdk.com>
2016-11-28 18:50:50 +0530

    More probs

--- src/com/algo/BubbleSortInt.java
+++ src/com/algo/BubbleSortInt.java
@@ -1,15 +1,23 @@
 package com.algo;
 
+import java.util.Arrays;
+
 /**
  * Created by Naveen Kumar .A on 11/28/16.
  */
 public class BubbleSortInt {
 
     public static void main(String[] args) {
-        int[] i = {2,1,5,3,7,8};
+        int[] i = {5,2,1,3,8,7};
         for (int j = 0; j < i.length; j++) {
-
-
+            for (int k = 1; k <i.length-j ; k++) {
+                if(i[k-1] > i[k]){
+                    int temp = i[k-1];
+                    i[k-1] = i[k];
+                    i[k] = temp;
+                }
+            }
         }
+        System.out.println(Arrays.toString(i));
     }
 }
